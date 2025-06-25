5621c725ac8f027a2f5cb33699ffa0928d6c124b
hoang son <son666727@gmail.com>
2025-06-26 00:16:05 +0700

    push code

--- PTIT-CNTT3-CTDL&GT-SS04-B9.c
+++ PTIT-CNTT3-CTDL&GT-SS04-B9.c
@@ -1,3 +1,27 @@
-//
-// Created by admin on 6/20/2025.
-//
+#include <stdio.h>
+
+
+int timKiemDeQuy(int arr[], int n, int x, int index) {
+    if (index >= n) return -1;
+    if (arr[index] == x) return index;
+    return timKiemDeQuy(arr, n, x, index + 1);
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x;
+
+    printf("Nhap so can tim: ");
+    scanf("%d", &x);
+
+    int viTri = timKiemDeQuy(arr, n, x, 0);
+
+    if (viTri != -1) {
+        printf("Tim thay %d tai vi tri %d.\n", x, viTri);
+    } else {
+        printf("Khong tim thay %d trong mang.\n", x);
+    }
+
+    return 0;
+}
